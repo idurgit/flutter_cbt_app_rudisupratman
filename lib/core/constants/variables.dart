923b49fbc648345985677e7b11e21ba548fea0cb
Rudi <126459042+idurgit@users.noreply.github.com>
2023-12-04 22:17:34 +0700

    flutter meet 7 dan 8

---
+++ lib/core/constants/variables.dart
@@ -0,0 +1,4 @@
+class Variables {
+  static const String baseUrl =
+      'https://74a5-2001-448a-60c0-89cf-d970-14d2-b2ef-23f9.ngrok.io';
+}
---
+++ lib/data/datasources/auth_remote_datasource.dart
@@ -0,0 +1,60 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_cbt_tpa_app/core/constants/variables.dart';
+import 'package:flutter_cbt_tpa_app/data/datasources/auth_local_datasource.dart';
+import 'package:flutter_cbt_tpa_app/data/models/request/login_request_model.dart';
+import 'package:flutter_cbt_tpa_app/data/models/request/register_request_model.dart';
+import 'package:flutter_cbt_tpa_app/data/models/responses/auth_response_model.dart';
+import 'package:http/http.dart' as http;
+
+class AuthRemoteDatasource {
+  Future<Either<String, AuthResponseModel>> register(
+      RegisterRequestModel registerRequestModel) async {
+    final response = await http.post(
+      Uri.parse('${Variables.baseUrl}/api/register'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: registerRequestModel.toJson(),
+    );
+
+    if (response.statusCode == 200) {
+      return Right(AuthResponseModel.fromJson(response.body));
+    } else {
+      return const Left('Register gagal');
+    }
+  }
+
+  Future<Either<String, String>> logout() async {
+    final authData = await AuthLocalDatasource().getAuthData();
+    final response = await http.post(
+      Uri.parse('${Variables.baseUrl}/api/logout'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ${authData.accessToken}',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return const Right('Logout berhasil');
+    } else {
+      return const Left('Logout gagal');
+    }
+  }
+
+  Future<Either<String, AuthResponseModel>> login(
+      LoginRequestModel data) async {
+    final response = await http.post(
+      Uri.parse('${Variables.baseUrl}/api/login'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept' : 'application/json',
+      },
+      body: data.toJson());
+
+    if (response.statusCode == 200) {
+      return Right(AuthResponseModel.fromJson(response.body));
+    } else {
+      return const Left('Login gagal');
+    }
+  }
+}
